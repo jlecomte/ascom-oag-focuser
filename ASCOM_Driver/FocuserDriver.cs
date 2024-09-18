54a6499e8a0a2fca45fa6a6ebd629ec2a65cfdc0
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2024-09-18 23:53:31 +0300

    Fix exception on writing Last COM Port setting to ASCOM profile
    Writing the "Last COM Port" value to the ASCOM profile of the focuser
    throws an exception, because the device type of the profile has not been
    set to "Focuser", so it is "Telescope" by default.
    
    This means that if auto-detecting the COM port is off in the focuser
    app, the connection always fails.
    
    The device type was being set inside an "if" clause in the code, meaning
    it was never being set in the non-auto-detecting case.

--- ASCOM_Driver/FocuserDriver.cs
+++ ASCOM_Driver/FocuserDriver.cs
@@ -234,7 +234,7 @@ namespace ASCOM.DarkSkyGeek
 
                     Debug.Assert(objSerial == null);
 
-                    using (Profile driverProfile = new Profile())
+                    using (Profile driverProfile = new Profile() { DeviceType = "Focuser" })
                     {
                         Serial serial = null;
 
@@ -242,8 +242,6 @@ namespace ASCOM.DarkSkyGeek
 
                         if (autoDetectComPort)
                         {
-                            driverProfile.DeviceType = "Focuser";
-
                             // See if the last successfully connected COM port can be used first...
                             // This is a performance optimization that significantly reduces the time it takes to connect!
                             string lastComPort = driverProfile.GetValue(driverID, lastComPortProfileName, string.Empty, string.Empty);
